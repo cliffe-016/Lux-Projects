f95656b19e4b66758e64d67162077b9f62a691ef
cliffe <oluochcliffe13@gmail.com>
2026-04-27 23:51:53 +0300

    Configure massive api dag using operators

--- python/massive_api/massive_stocks.py
+++ python/massive_api/massive_stocks.py
@@ -2,72 +2,84 @@ from massive import RESTClient
 import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
-from airflow.sdk import dag, task
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import json
+import time
+
+# Write ETL logic firsst
+def extract(**kwargs):
+    client = RESTClient("LKnpEkBqcVOJhkcOwMtHEDsJmuUMpliY")
+
+    # Store companies in a list
+    companies = ["AAPL", "GOOG", "TSLA", "AMZN", "NFLX"]
+    data = []
+
+    # Iterate throguh companies list to extract data
+    for ticker in companies:
+        request = client.get_daily_open_close_agg(
+            ticker,
+            "2025-04-22",
+            adjusted="true",
+        )
+        data.append(request)
+    
+    # Rate limting error to elminate 429 error
+    time.sleep(2)
+    # Convert list to dataframe first to flatten the list
+    raw_df = pd.DataFrame(data)
+    
+    # Convert to json
+    raw_data = json.loads(raw_df.to_json(orient="records"))
+
+    kwargs["ti"].xcom_push(key="raw_json", value=raw_data)
+
+def transform(**kwargs):
+    data_transform = kwargs["ti"].xcom_pull(task_ids="extract", key="raw_json")
+    df_transform = pd.DataFrame(data_transform)
+    # Rename date column
+    df_transform = df_transform.rename(columns={"from_":"date"})
+    # Drop none valued column
+    df_transform = df_transform.drop(["otc"], axis=1) 
+    # Add price change column
+    df_transform["price_change"] = df_transform["close"] - df_transform["open"]
+
+    transformed_data = json.loads(df_transform.to_json(orient="records"))
+    kwargs["ti"].xcom_push(key="transformed_json", value=transformed_data)
+
+def load(**kwargs):
+    transformed_data = kwargs["ti"].xcom_pull(task_ids="transform", key="transformed_json")
+    # convert transformed data to df
+    df_load = pd.DataFrame(transformed_data)
+
+    # Define sqlalchemy engine
+    engine = create_engine("postgresql+psycopg2://lux_user:1234@127.0.0.1:5432/massive_db")
+
+    # Load
+    df_load.to_sql("tech_stocks", con=engine, if_exists="append", index=False)
+    print("Rows:", len(df_load), "loaded")
+
 
 # Define dag arguments
-@dag(
+with DAG(
+    "cliffe_stocks_dag_ops",
     start_date=datetime(2026, 4, 24),
     schedule='@hourly',
-    catchup=False
-    )
+    catchup=False,
+    max_active_runs=1
+    ) as dag:
+        task_extract = PythonOperator(
+            task_id="extract_data", 
+            python_callable=extract)
 
-# Define dag id
-def cliffe_stocks_dag():
-    @task()
-    def extract():
-        client = RESTClient("lppjfqABG22zKmPuspOyep56wMdB0772")
+        task_transform = PythonOperator(
+            task_id="transform_data",
+            python_callable=transform)
 
-        # Store companies in a list
-        companies = ["AAPL", "GOOG", "TSLA", "AMZN", "NFLX"]
-        data = []
+        task_load = PythonOperator(
+            task_id="load_data",
+            python_callable=load)
 
-        # Iterate throguh companies list to extract data
-        for ticker in companies:
-            request = client.get_daily_open_close_agg(
-                ticker,
-                "2025-04-22",
-                adjusted="true",
-            )
-            data.append(request)
 
-        # Convert list to dataframe first to flatten the list
-        raw_df = pd.DataFrame(data)
-        
-        # Convert to json
-        raw_data = json.loads(raw_df.to_json(orient="records"))
-
-        return raw_data
-
-    @task()
-    def transform(raw_data):
-        df_transform = pd.DataFrame(raw_data)
-        
-        # Rename date column
-        df_transform = df_transform.rename(columns={"from_":"date"})
-        # Drop none valued column
-        df_transform = df_transform.drop(["otc"], axis=1)   
-        # Add price change column
-        df_transform["price_change"] = df_transform["close"] - df_transform["open"]
-
-        transformed_data = json.loads(df_transform.to_json(orient="records"))
-        return transformed_data
-
-    @task()
-    def load(transformed_data):
-        # convert transformed data to df
-        df_load = pd.DataFrame(transformed_data)
-
-        # Define sqlalchemy engine
-        engine = create_engine("postgresql+psycopg2://lux_user:1234@127.0.0.1:5432/massive_db")
-
-        # Load
-        df_load.to_sql("tech_stocks", con=engine, if_exists="append", index=False)
-        print("Rows:", len(df_load), "loaded")
-
-    raw_data=extract()
-    transformed_data = transform(raw_data)
-    load(transformed_data)
-
-cliffe_stocks_dag()
+        task_extract >> task_transform >> task_load
